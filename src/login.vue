463d0597b7e09c4e66402bab17625c708575ac59
ylmc <747269511@qq.com>
2025-05-26 09:41:58 +0800

    init

---
+++ src/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="test1">
+        abc
+        <div class="test2">def</div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.test1 {
+    .test2 {
+        height: 100px;
+        width: 100px;
+        background-color: red;
+    }
+}
+</style>
